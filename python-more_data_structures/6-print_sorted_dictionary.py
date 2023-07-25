b9b68a632fb57384978ceb5e63fe50dd7fae9c72
sunny200745 <sunny.200745@gmail.com>
2023-07-25 11:06:09 -0700

    Commit

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    prints a dictionary by ordered keys. keys assumed to be strings
+    """
+    [print(f"{key}: {a_dictionary[key]}") for key in sorted(a_dictionary)]
